571501711e476086138f78532d22e5d9ab4afc9d
JuditBarabas <62601033+JuditBarabas@users.noreply.github.com>
2020-10-25 17:40:48 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function rant(message) {
+    console.log(message.toUpperCase());
+    console.log(message.toUpperCase());
+    console.log(message.toUpperCase());
+}
+
+
+console.log('Just to test it:');
+rant('I hate beets');
